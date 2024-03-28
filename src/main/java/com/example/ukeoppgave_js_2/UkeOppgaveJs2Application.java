d87aac6022ed0bc5f1cc79d8b1f8d1962b184c36
47462 <coskun.aydn@gmail.com>
2024-03-28 17:01:38 +0100

    Initial commit

---
+++ src/main/java/com/example/ukeoppgave_js_2/UkeOppgaveJs2Application.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgave_js_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkeOppgaveJs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkeOppgaveJs2Application.class, args);
+    }
+
+}
